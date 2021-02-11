2ad109857231f56266717ba0de0039880259cada
joshuarg007 <42895544+joshuarg007@users.noreply.github.com>
2021-02-10 20:08:45 -0700

    Mapped Controller to Keyboard for every individual input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody playerRb;
+
+    public Transform gameCamera;
+
+    public float speed = 4.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+        turnCamera();
     }
+
+    public void turnCamera() { 
+    
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        float rotationalInput = Input.GetAxis("MouseX");
+
+        float rightTrigger = Input.GetAxis("RT");
+
+        float leftTrigger = Input.GetAxis("LT");
+
+        bool leftBumper = Input.GetButtonDown("LB");
+
+        bool rightBumper = Input.GetButtonDown("RB");
+
+        bool jump = Input.GetButtonDown("Jump");
+
+        bool fire1 = Input.GetButtonDown("Fire1");
+
+        bool fire2 = Input.GetButtonDown("Fire2");
+
+        bool fire3 = Input.GetButtonDown("Fire3");
+
+        if(Input.GetButtonDown("ToggleMenu"))
+        {
+            print("Pressed");
+        }
+
+
+        Vector3 direction = transform.forward * verticalInput + transform.right * horizontalInput;
+
+    }
+
 }
